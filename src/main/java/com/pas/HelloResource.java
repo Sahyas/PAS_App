39eb828bfa8dae86c2d58067a7a347a5ffb5b63f
Sahyas <sahya230201@gmail.com>
2022-10-09 18:52:41 +0200

    Revert "test first commit"
    This reverts commit 550b2188aa77e8b8051e29243dbf1ac2859234b7.

--- src/main/java/com/pas/HelloResource.java
+++ src/main/java/com/pas/HelloResource.java
@@ -9,6 +9,6 @@ public class HelloResource {
     @GET
     @Produces("text/plain")
     public String hello() {
-        return "Hello, World!/test change";
+        return "Hello, World!";
     }
 }
